63a9dc28a5a460f8454b9fb73ce6c90a9e0c73fe
Hiep <hiepnt.adnetplus@gmail.com>
2020-02-12 09:52:21 +0700

    change something

--- docbao/app/src/main/java/com/hiepsummer/docbao/ui/home/HomeViewModel.java
+++ docbao/app/src/main/java/com/hiepsummer/docbao/ui/home/HomeViewModel.java
@@ -15,4 +15,12 @@ public class HomeViewModel extends ViewModel {
     public LiveData<String> getText() {
         return mText;
     }
+
+    /*
+    * LiveData là một lớp nắm giữ dữ liệu và cho phép dữ liệu đó có thể quan sát được.
+    * Lưu ý : Đảm bảo rằng lưu trữ LiveData object ở trong ViewModel object, tránh lưu trữ ở activities và fragments vì
+        - Tránh phình to activities, fragments. Activity và Fragment chỉ đảm bảo nhiệm vụ hiển thị data chứ không lưu trữ data
+        - Lưu trữ LiveData objects ở ViewModel đảm bảo LiveData objects tồn tại ngay cả khi có sự thay đổi về config.
+    *
+    * */
 }
