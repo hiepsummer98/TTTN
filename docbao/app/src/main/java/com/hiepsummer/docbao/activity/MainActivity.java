47106290d321214e3cda2155bd3cd52a6374427d
Hiep <hiepnt.adnetplus@gmail.com>
2020-02-21 23:02:50 +0700

    change item News

--- docbao/app/src/main/java/com/hiepsummer/docbao/activity/MainActivity.java
+++ docbao/app/src/main/java/com/hiepsummer/docbao/activity/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         if (!checkConnect(MainActivity.this)) builder(MainActivity.this).show();
         else {
